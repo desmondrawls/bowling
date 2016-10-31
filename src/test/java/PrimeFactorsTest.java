0e43ed4b29c6667c0857dab4bc6879524ce33c67
Pivotal <pivotal@9735.local>
2016-10-31 16:25:29 +0100

    Finish prime factors.

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,6 +1,7 @@
 import org.hamcrest.Matcher;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -15,9 +16,23 @@ public class PrimeFactorsTest {
     @Test
     public void primeFactors() throws Exception {
         assertThat(primeFactorsOf(1), isListOf());
+        assertThat(primeFactorsOf(2), isListOf(2));
+        assertThat(primeFactorsOf(3), isListOf(3));
+        assertThat(primeFactorsOf(4), isListOf(2,2));
+        assertThat(primeFactorsOf(5), isListOf(5));
+        assertThat(primeFactorsOf(6), isListOf(2,3));
+        assertThat(primeFactorsOf(7), isListOf(7));
+        assertThat(primeFactorsOf(8), isListOf(2,2,2));
+        assertThat(primeFactorsOf(9), isListOf(3,3));
     }
 
     private List<Integer> primeFactorsOf(int number) {
-        return null;
+        ArrayList<Integer> primeFactors = new ArrayList();
+        for(int divisor = 2; number > 1; divisor++){
+            for(;number % divisor == 0; number /= divisor){
+              primeFactors.add(divisor);
+            }
+        }
+        return primeFactors;
     }
 }
